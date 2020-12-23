be0e34025ab76c9677fadcfd05bc97bedd96a5cb
Fission <fizzybuzz@protonmail.com>
2020-12-23 00:52:52 -0500

    `rrr` crate initial commit

---
+++ crates/rrr/src/settings.rs
@@ -0,0 +1,36 @@
+#[cfg(feature = "serde1")]
+use serde::{Deserialize, Serialize};
+
+/// Stores RRR settings to play charts with.
+#[cfg_attr(feature = "serde1", derive(Serialize, Deserialize))]
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default)]
+pub struct Settings {}
+
+/// A builder for a [Settings](Settings) instance.
+#[cfg_attr(feature = "serde1", derive(Serialize, Deserialize))]
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default)]
+pub struct SettingsBuilder {}
+
+impl SettingsBuilder {
+    #[must_use]
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    #[must_use]
+    #[allow(clippy::unused_self)]
+    pub fn build(&self) -> Settings {
+        Settings {}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn settings_builder() {
+        let settings_builder = SettingsBuilder::new();
+        let _settings = settings_builder.build();
+    }
+}
---
+++ docs/src/SUMMARY.md
@@ -0,0 +1,6 @@
+# Summary
+
+- [Rust Rust Revolution](README.md)
+- [Technical Prototypes](prototypes/README.md)
+  - [SN01](prototypes/sn01.md)
+  - [SN02](prototypes/sn02.md)
