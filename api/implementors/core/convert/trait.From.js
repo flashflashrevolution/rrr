d4599167e041a0dcfa0a2282033cdee4b03f1ef0
fizzybuzz <fizzybuzz@users.noreply.github.com>
2021-01-26 06:36:31 +0000

    deploy: 9e86ac9ae63567f3cbba968c20260da61b95bd84

--- api/implementors/core/convert/trait.From.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["rrr"] = [{"text":"impl From&lt;Chart&gt; for CompiledChart","synthetic":false,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
