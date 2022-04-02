dcdfa356c91780c8cf560c93615fb064b4498e59
Zageron <hello@zageron.ca>
2022-04-02 11:25:58 -0700

    Upgrade dependencies and remove egui

--- crates/render/src/main.rs
+++ crates/render/src/main.rs
@@ -2,7 +2,7 @@ use macroquad::prelude::*;
 
 fn window_conf() -> Conf {
     Conf {
-        window_title: "egui with macroquad".to_owned(),
+        window_title: "macroquad".to_owned(),
         high_dpi: true,
         ..Default::default()
     }
@@ -16,32 +16,15 @@ fn draw_primitives() {
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    let mut draw_primitives_after_egui = false;
-
     loop {
         clear_background(RED);
-
-        egui_macroquad::ui(|egui_ctx| {
-            egui::Window::new("egui ❤ macroquad").show(egui_ctx, |ui| {
-                ui.checkbox(
-                    &mut draw_primitives_after_egui,
-                    "Draw macroquad primitives after egui",
-                );
-            });
-        });
 
         set_camera(&Camera2D {
             zoom: vec2(1., screen_width() / screen_height()),
             ..Default::default()
         });
 
-        if !draw_primitives_after_egui {
-            draw_primitives();
-        }
-        egui_macroquad::draw();
-        if draw_primitives_after_egui {
-            draw_primitives();
-        }
+        draw_primitives();
 
         next_frame().await
     }
