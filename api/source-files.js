57747f53d8dd91b6beb0c50dc63a3d135ac1c038
Zageron <Zageron@users.noreply.github.com>
2022-04-03 03:20:38 +0000

    deploy: flashflashrevolution/manifests@372c61a0bf5532c6e2923f8e4eb7584b5bead4b2

--- api/source-files.js
+++
@@ -1,4 +0,0 @@
-var N = null;var sourcesIndex = {};
-sourcesIndex["rrr"] = {"name":"","files":["chart.rs","lib.rs","settings.rs"]};
-sourcesIndex["rrr_render"] = {"name":"","files":["main.rs"]};
-createSourceSidebar();
