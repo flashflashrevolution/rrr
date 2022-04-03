a111c0b3318de790f48e8108f14aa948619e3514
Zageron <hello@zageron.ca>
2022-04-02 20:27:43 -0700

    Revert "deploy: flashflashrevolution/manifests@372c61a0bf5532c6e2923f8e4eb7584b5bead4b2"
    This reverts commit 57747f53d8dd91b6beb0c50dc63a3d135ac1c038.

---
+++ api/source-files.js
@@ -0,0 +1,4 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["rrr"] = {"name":"","files":["chart.rs","lib.rs","settings.rs"]};
+sourcesIndex["rrr_render"] = {"name":"","files":["main.rs"]};
+createSourceSidebar();
