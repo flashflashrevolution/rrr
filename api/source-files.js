3058ccd289f90887c7a0c73796dafed8479e167a
Zageron <Zageron@users.noreply.github.com>
2022-08-14 18:34:43 +0000

    deploy: d8ad4b1327783b36cfde2669fc302586fdb53867

--- api/source-files.js
+++ api/source-files.js
@@ -1,6 +1,6 @@
 var sourcesIndex = JSON.parse('{\
 "rrr_core":["",[["audio",[],["mod.rs","output.rs","player.rs","resampler.rs"]],["chart",[],["bpm.rs","chart_impl.rs","mod.rs","note.rs","parser.rs","swf_types.rs"]],["fetch",[],["mod.rs","native.rs","playlist.rs"]],["lerp",[],["mod.rs"]],["loaders",[],["mod.rs"]],["play",[],["actions.rs","judge.rs","mod.rs"]],["time",[["native_time",[],["mod.rs"]]],["mod.rs","time_trait.rs"]]],["lib.rs","record.rs","settings.rs","turntable.rs"]],\
-"rrr_head":["",[],["geo.rs","main.rs","noteskin.rs","sprites.rs","visibility.rs"]],\
+"rrr_head":["",[],["benchmark.rs","geo.rs","main.rs","noteskin.rs","sprites.rs","visibility.rs"]],\
 "worker":["",[],["worker.rs"]]\
 }');
 createSourceSidebar();
