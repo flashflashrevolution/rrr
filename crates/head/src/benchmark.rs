20fba532b85256a0d0f50bdfe27df65162f5bf1e
Zageron <hello@zageron.ca>
2022-08-14 12:02:27 -0700

    Fix skipped frames never adding up.

--- crates/head/src/benchmark.rs
+++ crates/head/src/benchmark.rs
@@ -21,17 +21,17 @@ impl BenchmarkData {
         self.frame_times.push(frame_time);
         self.min_frame_time = self.min_frame_time.min(frame_time);
         self.max_frame_time = self.max_frame_time.max(frame_time);
-        self.avg_frame_time = self
-            .frame_times
-            .iter()
-            .rev()
-            .take(60)
-            .fold(0.0, |a, b| a + b)
-            / 60.0;
 
-        if self.frame_times.len() > 60 && frame_time > self.avg_frame_time * 1.05 {
-            if let Some(res) = self.skipped_frames.checked_add(1) {
-                self.skipped_frames = res;
+        if self.frame_times.len() > 60 {
+            self.avg_frame_time = self.frame_times[self.frame_times.len() - 60..]
+                .iter()
+                .fold(0.0, |a, b| a + b)
+                / 60.0;
+
+            if frame_time > self.avg_frame_time * 1.1 {
+                if let Some(res) = self.skipped_frames.checked_add(1) {
+                    self.skipped_frames = res;
+                }
             }
         }
     }
