0142f2bc5bc7af0dd3b5024be6a68a0ec1041451
Zageron <hello@zageron.ca>
2022-08-14 21:00:01 -0700

    Update dependencies

--- crates/head/src/main.rs
+++ crates/head/src/main.rs
@@ -651,8 +651,6 @@ fn handle_keyboard_input(
         Comma => game.do_action(Direction::Down),
         Period => game.do_action(Direction::Up),
         Slash => game.do_action(Direction::Right),
-        G => window.set_cursor_grab(!modifiers.shift()).unwrap(),
-        H => window.set_cursor_visible(modifiers.shift()),
         Space => {
             do_toggle_game_state_debug(game);
         }
